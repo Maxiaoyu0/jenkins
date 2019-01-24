ce9011541e173a8d220ea7f138cdfd734ca99c6e
xiaoyuMA1 <ma-xiaoyu@hotmail.com>
2019-01-24 16:43:21 +0100

    add dockerfile

---
+++ src/main/java/fr/jenkins/controller/controller.java
@@ -0,0 +1,19 @@
+package fr.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program:
+ * @description:
+ * @author: M. Maxiaoyu
+ * @create: 2019-01-24 16:31
+ **/
+@RestController
+public class controller {
+    @RequestMapping("/")
+    public String affich(){
+        return "hello jenkins";
+    }
+}
